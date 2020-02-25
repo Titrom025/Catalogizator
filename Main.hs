4f0a9b81877417afd0cc8e73b4d54b217cc8c740
Titrom25 <titrom25@mail.ru>
2020-02-26 00:40:55 +0700

    Create cabal package, split up catalogizator.hs into separate files, added menu

---
+++ Main.hs
@@ -0,0 +1,18 @@
+-- {-# LANGUAGE BangPatterns #-}
+
+import System.Directory
+import MenuSource
+import FileHandler
+import System.IO
+import System.Console.ANSI
+
+
+main = do
+    getFilesInfo
+
+    setSGR [SetColor Foreground Vivid Yellow]
+    clearScreen 
+    print_menu
+
+    io_handler
+
---
+++ MenuSource.hs
@@ -0,0 +1,61 @@
+module MenuSource where 
+
+import FileHandler
+import System.Directory
+import System.Console.ANSI
+import System.IO
+
+getFilesInfo = do
+    existense <- doesFileExist ".files.csv"
+    if existense
+        then do
+            removeFile ".files.csv"
+        else do
+            return ()
+
+    scan_dir "."
+
+
+print_menu = do
+    putStr $ "#########################################" ++ "\n"
+    putStr $ "#                                       #" ++ "\n"
+    putStr $ "#     Insert one of these options:      #" ++ "\n"
+    putStr $ "#                                       #" ++ "\n"
+    putStr $ "#  1 - rescan current dir               #" ++ "\n"
+    putStr $ "#  2 - print file tree                  #" ++ "\n"
+    putStr $ "#  3 + filename - find doubles by name  #" ++ "\n"
+    putStr $ "#                                       #" ++ "\n"
+    putStr $ "#########################################" ++ "\n\n"
+ 
+
+io_handler = do
+    putStr "Option: "
+    hFlush stdout
+    command <- getLine
+ 
+    case command of 
+        "1" -> do 
+            clearScreen
+            setSGR [SetColor Foreground Vivid Green]
+            putStr "Rescan complete.\n\n"
+            setSGR [SetColor Foreground Vivid Yellow]
+            scan_dir "."
+            print_menu
+
+        "2" -> printTree "."
+        
+        "3" -> do 
+            putStr "\nInsert filename: "
+            hFlush stdout
+            filename <- getLine
+            findDoublesByName filename
+            return ()
+
+        otherwise -> do
+            clearScreen 
+            setSGR [SetColor Foreground Vivid Red]
+            putStr "Wrong option.\n\n"
+            setSGR [SetColor Foreground Vivid Yellow]
+            print_menu
+
+    io_handler
